692c3712b9d5732f0c5f04c9e55cc0fac4a6c827
Adharsh Suresh <adharshsuresh1997@gmail.com>
2021-12-05 10:37:59 +0530

    :sparkles: Added more frontend features
    Added more frontend features and made changes exp to restaurants.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Switch, Route, Link} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import AddReview from './components/addReview';
+import Restaurant from './components/restaurants';
+import RestaurantsList from './components/restaurantsList';
+import Login from './components/login';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [user,setUser] = useState(null);
+
+
+  async function login(user = null){
+    setUser(user);
+
+  }
+
+  async function logout(){
+    setUser(null);
+  }
+
+  return(<>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand" href="/restaurants">
+            Restaurant Reviews
+          </a>
+
+        {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button> */}
+        
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <Link to='/restaurants' className='nav-link' >
+                  Restaurants
+              </Link>
+            </li>
+            <li className="nav-item">
+              { user ? (
+                <a onClick={logout} className='nav-link' style={{cursor:"pointer"}} >
+                  Logout {user.name}
+                </a>
+              ) : (
+                 <Link to="/login" className="nav-link" >
+                   Login
+                 </Link>
+              )  } 
+              again
+            </li>
+          </ul>
+        
+      </nav>
+
+
+      <div className="container mt-3" >
+    
+        <Switch>
+          <Route exact path={['/','/restaurants']} component={RestaurantsList} />
+          <Route path = '/restaurants/:id/review' 
+                 render = { (props) => {return ( <AddReview {...props} user={user} />) } 
+                 }
+           />
+          <Route path='/restaurants/:id'
+                 render={ (props) => {return ( <Restaurant {...props} user={user} />) }
+                } 
+            />
+          <Route exact path='/login'
+                 render={ (props) => { return ( <Login {...props} login={login} />)}
+                }
+            />
+        </Switch>
+      
+      </div>
+      </div>  
+  </>);
 }
 
 export default App;
---
+++ frontend/src/services/restaurant.js
@@ -0,0 +1,35 @@
+import RestaurantsList from '../components/restaurantsList'
+import http from '../http-common'
+
+class RestaurantDataService {
+    getAll(page = 0){
+        return http.get(`?page=${page}`)
+    }
+
+    get(id){
+        return http.get(`/id/${id}`)
+    }
+
+    find(query, by = 'name', page = 0){
+        return http.get(`?${by}=${query}&page=${page}`)
+    }
+
+    createReview(data){
+        return http.post('/review',data)
+    }
+
+    updateReview(data){
+        return http.put('/review',data)
+    }
+
+    deleteReview(id){
+        return http.delete(`review?id=${id}`)
+    }
+
+    getCuisines(id){
+        return http.get(`/cuisines`)
+    }
+}
+
+export default new RestaurantDataService();
+
